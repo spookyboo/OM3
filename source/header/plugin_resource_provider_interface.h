040313aee116308ce25476d6db74698a112c8413
DESKTOP-50NP26A\Henry <spookyboo@hetnet.nl>
2017-06-05 20:57:25 +0200

    renamed

---
+++ source/header/plugin_resource_provider_interface.h
@@ -0,0 +1,47 @@
+/****************************************************************************
+**
+** Copyright (C) 2017, Henry van Merode
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+****************************************************************************/
+
+#ifndef __OM3_RESOURCE_PLUGIN_H__
+#define __OM3_RESOURCE_PLUGIN_H__
+
+#include "plugin_interface.h"
+
+/****************************************************************************
+ Defines the interface of a resource plugin. Resources are file system, subversion
+ git, database, etc.
+ ***************************************************************************/
+class MediaWidget;
+class _OM3Export PluginResourceProviderInterface : public PluginInterface
+{
+    public:
+        PluginResourceProviderInterface (void) {}
+        virtual ~PluginResourceProviderInterface (void) {}
+
+        /** @see PluginInterface
+        */
+        virtual const std::string& getType (void) const {return PLUGIN_TYPE_RESOURCE;}
+
+        /** Pass the information of a resource from the plugin to the main OM3 application
+        As a result, a pointer to a created MediaWidget is returned, which should be used by
+        the plugin.
+        @remarks If not used, just destroy the widget.
+        */
+        virtual MediaWidget* addResource (const AssetMetaData& assetMetaData) = 0;
+};
+
+#endif
